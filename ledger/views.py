b125db3d873bbeb2b6936881dcd220bede14d090
nikki <kristine.banzon@student.ateneo.edu>
2024-02-15 23:46:42 +0800

    Create a list of ingredients needed for the 2 recipes.

---
+++ ledger/views.py
@@ -0,0 +1,134 @@
+from django.shortcuts import render
+
+def recipes(request):    
+    contxt_main = {
+        "recipes": [
+            {
+                "name": "Recipe 1",
+                "ingredients": [
+                    {
+                        "name": "tomato",
+                        "quantity": "3pcs"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1kg"
+                    },
+                    {
+                        "name": "water",
+                        "quantity": "1L"
+                    },
+                    {
+                        "name": "sinigang mix",
+                        "quantity": "1 packet"
+                    }
+                ],
+                    "link": "/recipe/1"
+            },
+            {
+                "name": "Recipe 2",
+                "ingredients": [
+                    {
+                        "name": "garlic",
+                        "quantity": "1 head"
+                    },
+                    {
+                        "name": "onion",
+                        "quantity": "1pc"
+                    },
+                    {
+                        "name": "vinegar",
+                        "quantity": "1/2cup"
+                    },
+                    {
+                        "name": "water",
+                        "quanity": "1 cup"
+                    },
+                    {
+                        "name": "salt",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "whole black peppers",
+                        "quantity": "1 tablespoon"
+                    },
+                    {
+                        "name": "pork",
+                        "quantity": "1 kilo"
+                    }
+                ],
+                    "link": "/recipe/2"
+            }
+        ]
+    }
+    return render(request, 'recipes.html', contxt_main)
+
+def recipe1(request):
+    contxt_one = {
+        "name": "Recipe 1",
+        "ingredients": [
+            {
+                "name": "tomato",
+                "quantity": "3pcs"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "pork",
+                "quantity": "1kg"
+            },
+            {
+                "name": "water",
+                "quantity": "1L"
+            },
+            {
+                "name": "sinigang mix",
+                "quantity": "1 packet"
+            }
+        ],
+        "link": "/recipe/1"
+    }
+    return render(request, 'recipe.html', contxt_one)
+
+def recipe2(request):
+    contxt_two = {
+        "name": "Recipe 2",
+        "ingredients": [
+            {
+                "name": "garlic",
+                "quantity": "1 head"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "vinegar",
+                "quantity": "1/2cup"
+            },
+            {
+                "name": "water",
+                "quantity": "1 cup"
+            },
+            {
+                "name": "salt",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "whole black peppers",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "pork",
+                "quantity": "1 kilo"
+            }
+        ],
+        "link": "/recipe/2"
+    }
+    return render(request, 'recipe.html', contxt_two)
